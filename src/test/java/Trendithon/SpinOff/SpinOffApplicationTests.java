503256c2ca1531563dbe58b83fc0a02d683739ec
minseokKim6823 <alstjr6823@naver.com>
2024-02-05 17:21:20 +0900

    first commit

---
+++ src/test/java/Trendithon/SpinOff/SpinOffApplicationTests.java
@@ -0,0 +1,13 @@
+package Trendithon.SpinOff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpinOffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
