c501e5aa33aa112fa914015014a42b5c35e2a40a
minseokKim6823 <alstjr6823@naver.com>
2024-02-09 15:29:51 +0900

    Board 클래스 세팅

--- src/main/java/Trendithon/SpinOff/entity/Board.java
+++ src/main/java/Trendithon/SpinOff/entity/Board.java
@@ -1,8 +1,6 @@
 package Trendithon.SpinOff.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
@@ -14,7 +12,11 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long board_id;
-    private Long userId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id") // 매핑할 외부 엔티티의 컬럼명 지정
+//  private Member user;
+
     private String board_title;
     private String board_comment;
     private Integer board_like;
