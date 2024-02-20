cd151ae50255207ea37c96bcc81e3f3fce451e35
minseokKim6823 <alstjr6823@naver.com>
2024-02-20 14:24:56 +0900

    [0220] 검색기능+페이지네이션

--- src/main/java/Trendithon/SpinOff/domain/board/controller/BoardController.java
+++ src/main/java/Trendithon/SpinOff/domain/board/controller/BoardController.java
@@ -40,14 +40,6 @@ public class BoardController {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body("사용자를 찾을 수 없습니다.");
         }
 
-
-
-        // 작성자 이름 설정
-        //Long writerId = currentMember.getId();
-        //System.out.println(currentMember+"현재 맴버");
-        //System.out.println(currentMember.getMemberId()+"멤버아이디");
-        //boardDto.setWriter_id(writerId);
-        // BoardDto를 서비스로 전달
         boardService.save(boardDto);
 
         return ResponseEntity.ok("저장 성공");
