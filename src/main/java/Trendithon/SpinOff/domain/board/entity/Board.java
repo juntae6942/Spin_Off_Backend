3d88b42ee101f4e45a363b6da4adec46043e4d1d
potato <65760583+juntae6942@users.noreply.github.com>
2024-02-24 05:28:45 +0900

    add: DummyData 추가

--- src/main/java/Trendithon/SpinOff/domain/board/entity/Board.java
+++ src/main/java/Trendithon/SpinOff/domain/board/entity/Board.java
@@ -2,36 +2,38 @@ package Trendithon.SpinOff.domain.board.entity;
 
 import jakarta.persistence.*;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-    @Getter
-    @Setter
-    public class Board {
-        @Id
-        @GeneratedValue(strategy = GenerationType.AUTO)
-        private Long bno;// id
-        //@Column(length = 50,nullable = false)
-        private String projectName;// 제목
-        //@Column(length =20,nullable= false)
-        private String projectDescription;// 한줄 소개
-        //@Column(length= 1000,nullable = false)
-        private String projectBackground;//배경
-        //@Column(length=100,nullable = false)
-        private String projectFeatures;//주요기능
-        private String projectImage;// 이미지 URL
-        private String distribution;//배포 URL
-        private String github;//깃허브 링크
-        private String member1;
-        private String member2;
-        private String member3;
-        private String member4;
-        private String member5;
-        private String category;//프로젝트 카테고리
-        private Integer boardLike;// 좋아요
-        private String writer;
-    }
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long bno;// id
+    //@Column(length = 50,nullable = false)
+    private String projectName;// 제목
+    //@Column(length =20,nullable= false)
+    private String projectDescription;// 한줄 소개
+    //@Column(length= 1000,nullable = false)
+    private String projectBackground;//배경
+    //@Column(length=100,nullable = false)
+    private String projectFeatures;//주요기능
+    private String projectImage;// 이미지 URL
+    private String distribution;//배포 URL
+    private String github;//깃허브 링크
+    private String member1;
+    private String member2;
+    private String member3;
+    private String member4;
+    private String member5;
+    private String category;//프로젝트 카테고리
+    private Integer boardLike;// 좋아요
+    private String writer;
+}
 
--- src/main/java/Trendithon/SpinOff/init/initData.java
+++ src/main/java/Trendithon/SpinOff/init/initData.java
@@ -5,6 +5,7 @@ import static Trendithon.SpinOff.domain.jobposting.entity.type.ExperienceLevel.N
 
 import Trendithon.SpinOff.domain.board.dto.BoardDto;
 import Trendithon.SpinOff.domain.board.entity.Board;
+import Trendithon.SpinOff.domain.board.repository.BoardRepository;
 import Trendithon.SpinOff.domain.board.service.BoardService;
 import Trendithon.SpinOff.domain.jobposting.entity.JobPosting;
 import Trendithon.SpinOff.domain.jobposting.repository.JobPostingJpaRepository;
@@ -26,32 +27,53 @@ public class initData {
     private final BoardService boardService;
     private final JobPostingService jobPostingService;
     private final JobPostingJpaRepository jobPostingJpaRepository;
+    private final BoardRepository boardRepository;
+
     @PostConstruct
     public void init() throws JsonProcessingException {
         memberService.signUp(SignUpDto.builder()
                         .memberId("admin")
                         .name("tester")
-                        .password("test123")
+                        .password("test1234")
                         .email("coy@naver.com")
                 .build());
         memberService.signUp(SignUpDto.builder()
                 .memberId("test1")
                 .name("tester")
-                .password("test123")
+                .password("test1234")
                 .email("coy@naver.com")
                 .build());
         memberService.signUp(SignUpDto.builder()
                 .memberId("test2")
                 .name("tester")
-                .password("test123")
+                .password("test1234")
                 .email("coy@naver.com")
                 .build());
         memberService.signUp(SignUpDto.builder()
                 .memberId("test3")
                 .name("tester")
-                .password("test123")
+                .password("test1234")
                 .email("coy@naver.com")
                 .build());
+        memberService.signUp(SignUpDto.builder()
+                .memberId("khayoung")
+                .email("coy@naver.com")
+                .password("test1234")
+                .name("권하영")
+                .build());
+        memberService.signUp(SignUpDto.builder()
+                .memberId("doteeth")
+                .email("coy@naver.com")
+                .name("고도희")
+                .password("test1234")
+                .build());
+        memberService.signUp(SignUpDto.builder()
+                .memberId("pookey1104")
+                .email("coy@naver.com")
+                .name("김서윤")
+                .password("test1234")
+                .build());
+
         JobPosting jobPosting = JobPosting.builder()
                 .logoUrl("https://s3.ap-northeast-2.amazonaws.com/trendithon.enterprise.logo/%EA%B5%90%EB%B3%B4%EC%83%9D%EB%AA%85%EB%B3%B4%ED%97%98.jpg")
                 .companyName("교보생명보험")
@@ -161,6 +183,126 @@ public class initData {
                 .build();
         jobPostingJpaRepository.save(jobPosting8);
 
+        boardRepository.save(Board.builder()
+                        .projectImage("https://trendithonfile.s3.ap-northeast-2.amazonaws.com/ToTree.png")
+                        .projectFeatures("나무 이쁘죠?")
+                        .projectName("ToTree")
+                        .distribution("https://ToTree.net")
+                        .boardLike(9)
+                        .projectDescription("크리스마스 트리가 이쁘네요")
+                        .member1("test1")
+                        .member2("test2")
+                        .member3("test3")
+                        .github("https://github.com")
+                        .category("category")
+                        .projectBackground("나무를 꾸며보자")
+                        .writer("김서윤")
+                .build());
+        boardRepository.save(Board.builder()
+                .projectImage("https://trendithonfile.s3.ap-northeast-2.amazonaws.com/Savior.png")
+                .projectFeatures("여행")
+                .projectName("Savior")
+                .distribution("https://Savior.com")
+                .boardLike(5)
+                .projectDescription("싸게 가는 여행")
+                .member1("test1")
+                .member2("test2")
+                .member3("test3")
+                .github("https://github.com")
+                .category("category")
+                .projectBackground("여행 경비를 절약하자")
+                .writer("김서윤")
+                .build());
+        boardRepository.save(Board.builder()
+                .projectImage("https://trendithonfile.s3.ap-northeast-2.amazonaws.com/Proust.jpg")
+                .projectFeatures("향수")
+                .projectName("Proust")
+                .distribution("https://Proust.org")
+                .boardLike(13)
+                .projectDescription("향수냄새 알고싶어?")
+                .member1("test1")
+                .member2("test2")
+                .member3("test3")
+                .github("https://github.com")
+                .category("category")
+                .projectBackground("향수 냄새 좋다 어떤거야?")
+                .writer("김서윤")
+                .build());
+        boardRepository.save(Board.builder()
+                .projectImage("https://trendithonfile.s3.ap-northeast-2.amazonaws.com/KakaoTalk_20240218_171344837+(1).png")
+                .projectFeatures("옷")
+                .projectName("chulsu's closet")
+                .distribution("https://chulsuscloset.com")
+                .boardLike(10)
+                .projectDescription("옷빌려줘")
+                .member1("test1")
+                .member2("test2")
+                .member3("test3")
+                .github("https://github.com")
+                .category("category")
+                .projectBackground("옷이 없는데 돈도 없어")
+                .writer("김서윤")
+                .build());
+        boardRepository.save(Board.builder()
+                .projectImage("https://trendithonfile.s3.ap-northeast-2.amazonaws.com/%EB%9B%B0%EC%8A%88.png")
+                .projectFeatures("건강/운동")
+                .projectName("뛰슈")
+                .distribution("https://main--runandrun.netlify.app/signin")
+                .boardLike(10)
+                .projectDescription("다같이 건강챙기면서 살자")
+                .member1("test1")
+                .member2("test2")
+                .member3("test3")
+                .github("https://github.com/LikeLionRunnersApp")
+                .category("category")
+                .projectBackground("건강챙기면서 공부하자는 뜻에서 시작한 프로젝트")
+                .writer("박준태")
+                .build());
+        boardRepository.save(Board.builder()
+                .projectImage("https://trendithonfile.s3.ap-northeast-2.amazonaws.com/%EB%8B%A4%EC%9A%B4%EB%A1%9C%EB%93%9C.png")
+                .projectFeatures("축제 홍보")
+                .projectName("상명대 축제")
+                .distribution("http://상명대")
+                .boardLike(0)
+                .projectDescription("상명대 축제 홍보")
+                .member1("test1")
+                .member2("test2")
+                .member3("test3")
+                .github("https://github.com")
+                .category("category")
+                .projectBackground("축제 홍보")
+                .writer("김민석")
+                .build());
+        boardRepository.save(Board.builder()
+                .projectImage("https://trendithonfile.s3.ap-northeast-2.amazonaws.com/%EC%95%88%EB%8F%84.png")
+                .projectFeatures("대중교통")
+                .projectName("안도")
+                .distribution("https://안도택시.net")
+                .boardLike(4)
+                .projectDescription("택시 합승")
+                .member1("test1")
+                .member2("test2")
+                .member3("test3")
+                .github("https://github.com/안도")
+                .category("category")
+                .projectBackground("택시 합승으로 안전하게 귀가하기")
+                .writer("김서윤")
+                .build());
+        boardRepository.save(Board.builder()
+                .projectImage("https://trendithonfile.s3.ap-northeast-2.amazonaws.com/%ED%8B%B0%EB%B0%8D.png")
+                .projectFeatures("팀이름 작명")
+                .projectName("티밍")
+                .distribution("https://티밍.con")
+                .boardLike(4)
+                .projectDescription("팀이름 정해주기")
+                .member1("test1")
+                .member2("test2")
+                .member3("test3")
+                .github("https://github.com/티밍")
+                .category("category")
+                .projectBackground("팀프로젝트 이름을 정해줄게!")
+                .writer("김서윤")
+                .build());
     }
 
 }
