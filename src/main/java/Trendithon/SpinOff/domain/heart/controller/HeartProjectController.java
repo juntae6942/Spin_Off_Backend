13f0f3267de5ca3cb9f01721bd9906a187ba0206
potato <65760583+juntae6942@users.noreply.github.com>
2024-02-25 21:07:53 +0900

    git ignore 수정

--- src/main/java/Trendithon/SpinOff/domain/heart/controller/HeartProjectController.java
+++ src/main/java/Trendithon/SpinOff/domain/heart/controller/HeartProjectController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class HeartProjectController {
 
     private final HeartProjectRepository heartProjectRepository;
